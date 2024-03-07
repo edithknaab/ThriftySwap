0d8dc1ec8455f1da5340b90467e2293500d23ec6
edithknaab <kwadannaabe1@southernct.edu>
2024-03-06 20:33:15 -0500

    fixed inventory records

--- models.py
+++ models.py
@@ -45,40 +45,22 @@ class IntakeTransaction(db.Model):
     inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'))
     item_name = db.Column(db.String(50))
     quantity = db.Column(db.Integer)
-    user = db.Column(db.String)  # Assuming you have a user field
+    user = db.Column(db.String)  
     timestamp = db.Column(db.DateTime)
     donor_info = db.Column(db.String)
 
-    # Define the relationship with the Inventory model
+    
     inventory = db.relationship('Inventory', back_populates='intake_transactions')
-
-    @property
-    def formatted_timestamp(self):
-        # Assuming you want to format the timestamp in the same way as in the OuttakeTransaction class
-        eastern_tz = pytz.timezone('America/New_York')
-        return self.timestamp.astimezone(eastern_tz).strftime('%I:%M %p')
-
 
 
 class OuttakeTransaction(db.Model):
     __tablename__ = 'outtake_transaction'
     id = db.Column(db.Integer, primary_key=True)
     inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'), nullable=False)
-    item_name = db.Column(db.String(50))  # Add this field to store the item name
+    item_name = db.Column(db.String(50))
     quantity = db.Column(db.Integer, nullable=False)
     donor_info = db.Column(db.String)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    # Define the relationship with the Inventory model
+    
     inventory = db.relationship('Inventory', back_populates='outtake_transactions')
-
-    @property
-    def timestamp_eastern(self):
-    # Convert timestamp to Eastern Time (ET)
-        eastern_tz = pytz.timezone('America/New_York')
-        return self.timestamp.astimezone(eastern_tz)
-
-    @property
-    def formatted_timestamp(self):
-        # Format the timestamp to show hours (12-hour clock), minutes, and AM/PM
-        return self.timestamp_eastern.strftime('%I:%M %p')
