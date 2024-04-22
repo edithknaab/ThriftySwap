3495e20eca5efd951db3617ef1a4b54698151cd4
Jonnathan Juela <Jonnathanjuela@gmail.com>
2024-04-21 22:55:30 -0400

    new email server

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SECRET_KEY = 'csc400'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'OwlGoSCSU@gmail.com'
-    MAIL_PASSWORD = 'rbjb dwxk lqly smlz'
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_DEFAULT_SENDER = ('ThriftyOwl', 'OwlGoSCSU@gmail.com')
+    MAIL_SERVER = 'smtp.office365.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = 'johnjuela@hotmail.com'
+    MAIL_PASSWORD = 'hehplkxugcssxesn'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_DEFAULT_SENDER = ('ThriftySwap', 'johnjuela@hotmail.com')
